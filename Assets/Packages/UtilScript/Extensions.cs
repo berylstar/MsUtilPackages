3864b14e562f4d88e1bbc7b5bb01ecb02d401e29
minsang <98025769+berylstar@users.noreply.github.com>
2025-12-14 20:05:55 +0900

    [FEAT] Extensions 추가 작업

--- Assets/Packages/UtilScript/Extensions.cs
+++ Assets/Packages/UtilScript/Extensions.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public static class Extensions
 {
+    #region UI
     /// <summary>
     /// 버튼 클릭 이벤트 초기화
     /// </summary>
@@ -21,4 +23,51 @@ public static class Extensions
         toggle.onValueChanged.RemoveAllListeners();
         toggle.onValueChanged.AddListener(changeAction);
     }
+    #endregion
+
+    #region Enumerate
+    /// <summary>
+    /// 배열 셔플
+    /// </summary>
+    public static void Shuffle<T>(this T[] array)
+    {
+        if (array == null || array.Length <= 1)
+            return;
+
+        int randomIndex;
+        T tempValue;
+
+        // Fisher-Yates 셔플
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            randomIndex = Random.Range(0, i + 1);
+
+            tempValue = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = tempValue;
+        }
+    }
+
+    /// <summary>
+    /// 리스트 셔플
+    /// </summary>
+    public static void Shuffle<T>(this List<T> list)
+    {
+        if (list == null || list.Count <= 1)
+            return;
+
+        int randomIndex;
+        T tempValue;
+
+        // Fisher-Yates 셔플
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            randomIndex = Random.Range(0, i + 1);
+
+            tempValue = list[i];
+            list[i] = list[randomIndex];
+            list[randomIndex] = tempValue;
+        }
+    }
+    #endregion
 }
