359a3297ca675d1611f8f4aa993f985d92ddd70c
Minsang Kim <minsang0610@gmail.com>
2026-02-04 13:24:52 +0900

    [CHORE]

---
+++ Assets/Tester/Tester.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Tester : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+
+        }
+    }
+}
