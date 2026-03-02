8086eab64c5a54726999c7fa41d41fa59caabe35
Minsang Kim <minsang0610@gmail.com>
2026-03-02 15:48:04 +0900

    [FIX] InspectorReadonly

--- Assets/MsPackages/InspectorReadonly/Editor/InspectorReadonlyDrawer.cs
+++ Assets/MsPackages/InspectorReadonly/Editor/InspectorReadonlyDrawer.cs
@@ -7,12 +7,23 @@ public class InspectorReadonlyDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        // 기존 값 복구
-        bool prevValue = GUI.enabled;
+        //// 기존 값 복구
+        //bool prevValue = GUI.enabled;
 
-        GUI.enabled = false;
+        //GUI.enabled = false;
+        //EditorGUI.PropertyField(position, property, label, true);
+        //GUI.enabled = prevValue;
+
+        EditorGUI.BeginDisabledGroup(true);
+
         EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = prevValue;
+
+        EditorGUI.EndDisabledGroup();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
     }
 }
 #endif
